2abe20836ab1e47e9b686da8df19c121a082e3a1
Jensen Bollen <jensen@Mac.iconos.be>
2025-09-16 15:28:57 +0200

    Fixed package vulnerabilities

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteTsPlugin from "vite-plugin-ts";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   server: {
@@ -9,10 +11,10 @@ export default defineConfig({
     // The plugin below is required for hot reloading React application.
     react(),
     // The plugin below is required for typescript support in Vite.
-    require("vite-plugin-ts")({
+    viteTsPlugin({
       tsconfig: "tsconfig.json",
     }),
     // The plugin below is required for importing SVG files as React components.
-    require("vite-plugin-svgr")(),
+    svgr(),
   ],
 });
